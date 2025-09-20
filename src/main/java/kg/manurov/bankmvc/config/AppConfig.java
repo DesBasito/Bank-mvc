43177eaee34f1a471ce798b8360be03336fe0cd1
DesBasito <116246777+DesBasito@users.noreply.github.com>
2025-09-20 16:48:13 +0600

    Remove locale configuration and message properties files
    Deleted `LocalConfig.java` and all `messages*.properties` files to simplify project structure and remove unnecessary localization support. This cleanup focuses on reducing maintenance complexity as localization is no longer used.

--- src/main/java/kg/manurov/bankmvc/config/AppConfig.java
+++ src/main/java/kg/manurov/bankmvc/config/AppConfig.java
@@ -6,8 +6,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.servlet.LocaleResolver;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @Configuration
 @EnableScheduling
@@ -16,9 +14,5 @@ public class AppConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
-    }
-    @Bean
-    public LocaleResolver localeResolver(){
-        return new SessionLocaleResolver();
     }
 }
