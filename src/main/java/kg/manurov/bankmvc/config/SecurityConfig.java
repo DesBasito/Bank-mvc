9363eb290d6a5a395a56af01f9f5cd4a10ad586c
DesBasito <116246777+DesBasito@users.noreply.github.com>
2025-09-22 00:14:58 +0600

    Make security changes and add layout templates
    Enabled CSRF disabling by uncommenting code in SecurityConfig. Added new Freemarker layout templates for admin and user interfaces. Simplified logout dropdown to a single link in the user profile navigation.

--- src/main/java/kg/manurov/bankmvc/config/SecurityConfig.java
+++ src/main/java/kg/manurov/bankmvc/config/SecurityConfig.java
@@ -3,10 +3,10 @@ package kg.manurov.bankmvc.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
@@ -28,7 +28,7 @@ public class SecurityConfig {
                 .logout(logout -> logout
                         .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
                         .permitAll())
-//                .csrf(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize -> authorize
 //                        .requestMatchers(HttpMethod.GET, "/","api/vacancies/paging").permitAll()
                         .anyRequest().permitAll());
